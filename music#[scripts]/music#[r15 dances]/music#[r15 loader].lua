cf96791c3158176283473bbb7e0288241f73ff39
Boxten <boxtenkeyes@gmail.com>
2025-07-24 15:51:13 -0400

    Update music#[r15 loader].lua

--- music#[scripts]/music#[r15 dances]/music#[r15 loader].lua
+++ music#[scripts]/music#[r15 dances]/music#[r15 loader].lua
@@ -21,6 +21,7 @@ settings["Respawn character"] = true --// Only should be disabled if your charac
 settings["Instant respawn"] = false --// Instant respawns the character, it will still wait the respawn time, but your character wont be dead. Requires: replicatesignal function. Enable if you want the feature
 settings["Hide HumanoidRootPart"] = false --// Enabled by default. when enabled, your chat bubble or name tag will not appear above your character, but you will have your character immortal in the Fencing arena.
 settings["PermaDeath fake character"] = false --// When enabled, when you die when the reanimate is on, you wont respawn. If you want respawn, set it to false
+settings["R15 Reanimate] = true
 
 settings["Names to exclude from transparency"] = {
     --[[ Example:
@@ -33,7 +34,7 @@ settings["Names to exclude from transparency"] = {
 local finished = false
 
 task.spawn(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxten-Keyes/music/refs/heads/main/music%23%5Bscripts%5D/music%23%5Br6%20dances%5D/music%23%5Bcurrent%20angle%20v2%5D.lua"))()
+	loadstring(game:HttpGet("https://github.com/Boxten-Keyes/music/raw/refs/heads/main/music%23%5Bscripts%5D/music%23%5Breanimators%5D/music%23%5Bcurrent%20angle%20v2%5D.lua"))()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxten-Keyes/music/refs/heads/main/music%23%5Bscripts%5D/music%23%5Br6%20dances%5D/music%23%5Br15%20dances%5D.lua"))()
 end)
 
