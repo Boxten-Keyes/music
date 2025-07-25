b17314072e399ac6600363a15a9cbf492cf2f64b
Boxten <boxtenkeyes@gmail.com>
2025-07-24 23:53:12 -0400

    Update music#[r15 fly].lua

--- music#[scripts]/music#[r15 fly].lua
+++ music#[scripts]/music#[r15 fly].lua
@@ -291,7 +291,7 @@ button.MouseButton1Click:Connect(function()
 	button.BackgroundColor3 = toggle and Color3.fromRGB(0, 255, 0) or Color3.fromRGB(0, 0, 0)
 	button.BorderColor3 = toggle and Color3.fromRGB(0, 0, 0) or Color3.fromRGB(255, 255, 255)
 	button.TextColor3 = toggle and Color3.fromRGB(0, 0, 0) or Color3.fromRGB(255, 255, 255)
-	if toggle then startFlying() else stopFlying() end
+	if toggle then startFlying() else stopFlying() resetanims() end
 end)
 
 plr.Character:WaitForChild("Humanoid").Died:Connect(function()
