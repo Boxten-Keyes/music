3c19bb73d06437487ff1170525271452f773656d
Boxten <boxtenkeyes@gmail.com>
2025-07-28 00:06:57 -0400

    Update music#[kill player].lua

--- music#[scripts]/music#[life in paradise]/music#[kill player].lua
+++ music#[scripts]/music#[life in paradise]/music#[kill player].lua
@@ -77,8 +77,8 @@ local function yeetPlayer(target)
 
 	-- Bring player to void
 	Workspace.FallenPartsDestroyHeight = 0 / 0
-	hrp.CFrame = CFrame.new(0, -1000, 0)
-	task.wait(0.2)
+	hrp.CFrame = CFrame.new(0, -2000, 0)
+	task.wait(0.4)
 
 	-- Unequip
 	stroller.Parent = LocalPlayer.Backpack
