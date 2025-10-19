aa37d47dac1a5c6f8e3ca479ef6a6cfffd342ba1
Boxten <boxtenkeyes@gmail.com>
2025-10-19 13:20:41 -0400

    Update music#[MM2SNS].lua

--- music#[scripts]/music#[murder mystery 2]/music#[MM2SNS].lua
+++ music#[scripts]/music#[murder mystery 2]/music#[MM2SNS].lua
@@ -1,18 +1,27 @@
-local Players = game:GetService("Players")  
-local LocalPlayer = Players.LocalPlayer  
+-------------------------------------------------------------------------------------------------------------------------------
 
-local function getCharacterRootPart(character)  
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+local plrs = game:GetService("Players")  
+local plr = plrs.LocalPlayer
+local onmobile = game:GetService("UserInputService").TouchEnabled
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+local function getroot(character)  
 	return character and (character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso"))  
 end  
 
-local function EquipGun()  
-	local character = LocalPlayer.Character  
-	local gun = LocalPlayer.Backpack:FindFirstChild("Gun") or (character and character:FindFirstChild("Gun"))  
-	if gun and gun.Parent == LocalPlayer.Backpack then  
+local function equipgun()  
+	local character = plr.Character  
+	local gun = plr.Backpack:FindFirstChild("Gun") or (character and character:FindFirstChild("Gun"))  
+	if gun and gun.Parent == plr.Backpack then  
 		gun.Parent = character  
 		task.delay(.17, function()  
 			if gun.Parent == character then  
-				gun.Parent = LocalPlayer.Backpack  
+				gun.Parent = plr.Backpack  
 			end  
 		end)  
 		return true  
@@ -22,24 +31,26 @@ local function EquipGun()
 	return false  
 end  
 
-local function hasKnife(player)  
+local function hasknife(player)  
 	return player.Backpack:FindFirstChild("Knife") or (player.Character and player.Character:FindFirstChild("Knife"))  
 end  
 
-local function getKnifePlayer()  
-	for _, player in pairs(Players:GetPlayers()) do  
-		if player ~= LocalPlayer and hasKnife(player) and player.Character then  
+local function getmurderer()  
+	for _, player in pairs(plrs:GetPlayers()) do  
+		if player ~= plr and hasknife(player) and player.Character then  
 			return player  
 		end  
 	end  
 	return nil  
-end  
+end
 
-local function shootAtKnifePlayer()  
-	local target = getKnifePlayer()  
+-------------------------------------------------------------------------------------------------------------------------------
+
+local function shootmurderer()  
+	local target = getmurderer()  
 	if target and target.Character then  
-		local targetRoot = getCharacterRootPart(target.Character)  
-		local originRoot = getCharacterRootPart(LocalPlayer.Character)  
+		local targetRoot = getroot(target.Character)  
+		local originRoot = getroot(plr.Character)  
 		if not targetRoot or not originRoot then return end  
 
 		local camera = workspace.CurrentCamera  
@@ -50,7 +61,7 @@ local function shootAtKnifePlayer()
 				[2] = targetRoot.Position,  
 				[3] = "AH2"  
 			}  
-			local gun = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Gun")  
+			local gun = plr.Character and plr.Character:FindFirstChild("Gun")  
 			if gun and gun:FindFirstChild("KnifeLocal") and gun.KnifeLocal:FindFirstChild("CreateBeam") then  
 				gun.KnifeLocal.CreateBeam.RemoteFunction:InvokeServer(unpack(args))  
 			end  
@@ -78,21 +89,23 @@ local function shootAtKnifePlayer()
 			[3] = "AH2"  
 		}  
 
-		local gun = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Gun")  
+		local gun = plr.Character and plr.Character:FindFirstChild("Gun")  
 		if gun and gun:FindFirstChild("KnifeLocal") and gun.KnifeLocal:FindFirstChild("CreateBeam") then  
 			gun.KnifeLocal.CreateBeam.RemoteFunction:InvokeServer(unpack(args))  
 		end  
 	end  
-end  
+end
+
+-------------------------------------------------------------------------------------------------------------------------------
 
 local roles
 local lastData
 
-local function isDifferent(t1, t2)
+local function difftable(t1, t2)
 	if typeof(t1) ~= typeof(t2) then return true end
 	if typeof(t1) ~= "table" then return t1 ~= t2 end
 	for k, v in pairs(t1) do
-		if isDifferent(v, t2[k]) then return true end
+		if difftable(v, t2[k]) then return true end
 	end
 	for k, v in pairs(t2) do
 		if t1[k] == nil then return true end
@@ -100,26 +113,27 @@ local function isDifferent(t1, t2)
 	return false
 end
 
-local function updateRoles()
+local function updroles()
 	local success, data = pcall(function() 
 		return game:GetService("ReplicatedStorage"):FindFirstChild("GetPlayerData", true):InvokeServer() 
 	end)
-	if success and data and isDifferent(data, lastData) then
+	if success and data and difftable(data, lastData) then
 		roles = data
 		lastData = data
 	end
-	task.delay(1.4, updateRoles)
+	task.delay(1.4, updroles)
 end
-updateRoles()
 
-local function IsAlive(player)
+updroles()
+
+local function isalive(player)
 	local roleData = roles and roles[player.Name]
 	return roleData and not roleData.Killed and not roleData.Dead
 end
 
-local function getRoleColor(player)
+local function getrole(player)
 	local roleData = roles and roles[player.Name]
-	if roleData and IsAlive(player) then
+	if roleData and isalive(player) then
 		local role = roleData.Role
 		if role == "Innocent" then
 			return Color3.fromRGB(0, 255, 0)
@@ -133,6 +147,8 @@ local function getRoleColor(player)
 	end
 	return Color3.fromRGB(0, 0, 0)
 end
+
+-------------------------------------------------------------------------------------------------------------------------------
 
 local nameloop = false
 
@@ -175,7 +191,7 @@ local function applyESPName(player, color)
 end
 
 local function clearESPName()
-	for _, player in pairs(Players:GetPlayers()) do
+	for _, player in pairs(plrs:GetPlayers()) do
 		local char = player.Character
 		if char then
 			local hrp = char:FindFirstChild("HumanoidRootPart")
@@ -187,15 +203,15 @@ local function clearESPName()
 	end
 end
 
-local function startESPName()
+local function startnameesp()
 	if nameloop then return end
 	nameloop = true
 
 	task.spawn(function()
 		while nameloop do
-			for _, player in pairs(Players:GetPlayers()) do
-				if player ~= LocalPlayer then
-					local color = getRoleColor(player)
+			for _, player in pairs(plrs:GetPlayers()) do
+				if player ~= plr then
+					local color = getrole(player)
 					applyESPName(player, color)
 				end
 			end
@@ -204,12 +220,13 @@ local function startESPName()
 	end)
 end
 
-local function stopESPName()
+local function stopnameesp()
 	nameloop = false
 	clearESPName()
 end
 
 local highlightUpdateLoop, highlightCleanupLoop, highloop
+
 local function applyHighlight(player, fillColor)
 	if not highloop then return end
 	local character = player.Character
@@ -224,7 +241,7 @@ local function applyHighlight(player, fillColor)
 end
 
 local function clearHighlight()
-	for _, player in pairs(Players:GetPlayers()) do
+	for _, player in pairs(plrs:GetPlayers()) do
 		local character = player.Character
 		if character then
 			local highlight = character:FindFirstChild("Highlight")
@@ -235,13 +252,13 @@ local function clearHighlight()
 	end
 end
 
-local function startESPHighlight()
+local function startplayeresp()
 	if highlightUpdateLoop or highlightCleanupLoop then return end highloop = true
 	highlightUpdateLoop = task.spawn(function()
 		while highloop do
-			for _, player in pairs(Players:GetPlayers()) do
-				if player == LocalPlayer then continue end
-				local color = getRoleColor(player)
+			for _, player in pairs(plrs:GetPlayers()) do
+				if player == plr then continue end
+				local color = getrole(player)
 				applyHighlight(player, color, color)
 			end
 			task.wait(1)
@@ -249,9 +266,9 @@ local function startESPHighlight()
 	end)
 	highlightCleanupLoop = task.spawn(function()
 		while highloop do
-			for _, player in pairs(Players:GetPlayers()) do
-				if player == LocalPlayer then continue end
-				if not IsAlive(player) then
+			for _, player in pairs(plrs:GetPlayers()) do
+				if player == plr then continue end
+				if not isalive(player) then
 					applyHighlight(player, Color3.fromRGB(0, 0, 0))
 				end
 			end
@@ -260,7 +277,7 @@ local function startESPHighlight()
 	end)
 end
 
-local function stopESPHighlight()
+local function stopplayeresp()
 	highloop = false
 	if highlightUpdateLoop then
 		task.cancel(highlightUpdateLoop)
@@ -332,36 +349,61 @@ function nogundropesp()
 	stopgunesploop()
 end
 
+-------------------------------------------------------------------------------------------------------------------------------
+
 local function touch(a, b)
 	firetouchinterest(a, b, 0)
 	firetouchinterest(a, b, 1)
 end
 
 function bringgun()
-	local c = LocalPlayer.Character
+	local c = plr.Character
 	local r = c and c:FindFirstChild("HumanoidRootPart")
 	local d = workspace:FindFirstChild("GunDrop", true)
 	if r and d then touch(r, d) end
 end
 
-game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
-	if gameProcessed then return end
-	if input.KeyCode == Enum.KeyCode.R then
-		if EquipGun() then  
-			for i = 1, 11 do  
-				task.spawn(function()  
-					shootAtKnifePlayer()  
-				end)  
-			end  
-		end
-	elseif input.KeyCode == Enum.KeyCode.E then
-		task.spawn(function() if not highloop then startESPHighlight() else stopESPHighlight() end end)
-		task.spawn(function() if not nameloop then startESPName() else stopESPName() end end)
-		task.spawn(function() if not gunespactive then gundropesp() else nogundropesp() end end)
-	elseif input.KeyCode == Enum.KeyCode.G then
-		task.spawn(function() bringgun() end)
+-------------------------------------------------------------------------------------------------------------------------------
+
+function stabsheriff()
+	local character = plr.Character or plr.CharacterAdded:Wait()
+	local backpack = plr:FindFirstChild("Backpack")
+
+	local knife = character:FindFirstChild("Knife") or (backpack and backpack:FindFirstChild("Knife"))
+
+	if knife and knife.Parent == backpack then
+		knife.Parent = character
+		repeat task.wait() until knife.Parent == character
 	end
-end)
+
+	if knife and knife.Parent == character then
+		local stabEvent = knife:FindFirstChild("Stab")
+
+		if stabEvent then
+			for _, target in ipairs(plrs:GetPlayers()) do
+				if target ~= plr then
+					local targetCharacter = target.Character
+					local targetBackpack = target:FindFirstChild("Backpack")
+
+					local hasGun = (targetCharacter and targetCharacter:FindFirstChild("Gun")) or 
+						(targetBackpack and targetBackpack:FindFirstChild("Gun"))
+
+					if hasGun then
+						local humanoidRootPart = targetCharacter and targetCharacter:FindFirstChild("HumanoidRootPart")
+
+						if humanoidRootPart then
+							stabEvent:FireServer("Slash")
+							firetouchinterest(humanoidRootPart, knife.Handle, 1)
+							firetouchinterest(humanoidRootPart, knife.Handle, 0)
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+-------------------------------------------------------------------------------------------------------------------------------
 
 local screenGui = Instance.new("ScreenGui")
 screenGui.ResetOnSpawn = false
@@ -369,28 +411,191 @@ screenGui.Parent = gethui() or game:GetService("CoreGui")
 if screenGui.Parent:FindFirstChild("MM2SNS") then screenGui.Parent:FindFirstChild("MM2SNS"):Destroy() end
 screenGui.Name = "MM2SNS"
 
-local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0, 307, 0, 99)
-frame.Position = UDim2.new(0, 14, 0, 10)
-frame.BackgroundTransparency = 0.5
-frame.BackgroundColor3 = Color3.new(0, 0, 0)
-frame.BorderSizePixel = 0
-frame.ZIndex = 2
-frame.Active = true
-frame.Draggable = true
-frame.Parent = screenGui
+if not onmobile then
+	game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
+		if gameProcessed then return end
+		if input.KeyCode == Enum.KeyCode.R then
+			if equipgun() then  
+				for i = 1, 11 do  
+					task.spawn(function()  
+						shootmurderer()  
+					end)  
+				end  
+			end
+		elseif input.KeyCode == Enum.KeyCode.E then
+			task.spawn(function() if not highloop then startplayeresp() else stopplayeresp() end end)
+			task.spawn(function() if not nameloop then startnameesp() else stopnameesp() end end)
+			task.spawn(function() if not gunespactive then gundropesp() else nogundropesp() end end)
+		elseif input.KeyCode == Enum.KeyCode.G then
+			task.spawn(function() bringgun() end)
+		elseif input.KeyCode == Enum.KeyCode.K then
+			task.spawn(function() stabsheriff() end)
+		end
+	end)
+	
+	local frame = Instance.new("Frame")
+	frame.Size = UDim2.new(0, 307, 0, 112)
+	frame.Position = UDim2.new(0, 14, 0, 10)
+	frame.BackgroundTransparency = 0.5
+	frame.BackgroundColor3 = Color3.new(0, 0, 0)
+	frame.BorderSizePixel = 0
+	frame.Active = true
+	frame.Draggable = true
+	frame.Parent = screenGui
 
-local label = Instance.new("TextLabel")
-label.Size = UDim2.new(1, -10, 1, 0)
-label.TextStrokeTransparency = 0
-label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-label.Position = UDim2.new(0, 4, 0, 6)
-label.BackgroundTransparency = 1
-label.TextColor3 = Color3.fromRGB(255, 255, 255)
-label.Font = Enum.Font.Code
-label.TextSize = 14
-label.ZIndex = 3
-label.TextXAlignment = Enum.TextXAlignment.Left
-label.TextYAlignment = Enum.TextYAlignment.Top
-label.Text = "Murder Mystery 2 | Simple Necessity Script\n\nControls:\nPress R to shoot the murderer.\nPress E to toggle ESP.\nPress G to grab the gun if dropped."
-label.Parent = frame
+	local label = Instance.new("TextLabel")
+	label.Size = UDim2.new(1, -10, 1, 0)
+	label.TextStrokeTransparency = 1
+	label.Position = UDim2.new(0, 4, 0, 6)
+	label.BackgroundTransparency = 1
+	label.TextColor3 = Color3.fromRGB(255, 255, 255)
+	label.Font = Enum.Font.Code
+	label.TextSize = 14
+	label.TextXAlignment = Enum.TextXAlignment.Left
+	label.TextYAlignment = Enum.TextYAlignment.Top
+	label.Text = "Murder Mystery 2 | Simple Necessity Script\n\nControls:\nPress R to shoot the murderer.\nPress E to toggle ESP.\nPress G to grab the gun if dropped.\nPress K to kill the sheriff."
+	label.Parent = frame
+else
+	function clik() 
+		task.spawn(function()
+			local s = Instance.new("Sound") 
+			s.SoundId = "rbxassetid://87152549167464"
+			s.Parent = workspace
+			s.Volume = 1.2 
+			s.TimePosition = 0.1 
+			s:Play() 
+		end)
+	end
+	
+	function repos(ui, w, h, off)
+		off = off or 0
+		local sw, sh = game.Workspace.CurrentCamera.ViewportSize.X, game.Workspace.CurrentCamera.ViewportSize.Y
+		local cx, cy = (sw - w) / 2, (sh - h) / 2 - 56
+		ui.Position = UDim2.new(0, cx + off, 0, cy)
+	end
+	
+	local shoot = Instance.new("TextButton")
+	shoot.Size = UDim2.new(0, 70, 0, 55)
+	shoot.TextStrokeTransparency = 1
+	repos(shoot, 70, 55)
+	shoot.BackgroundTransparency = 0.3
+	shoot.TextColor3 = Color3.fromRGB(255, 255, 255)
+	shoot.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	shoot.Font = Enum.Font.Code
+	shoot.BorderSizePixel = 0
+	shoot.TextSize = 13
+	shoot.TextXAlignment = Enum.TextXAlignment.Center
+	shoot.TextYAlignment = Enum.TextYAlignment.Center
+	shoot.Active = true
+	shoot.Draggable = true
+	shoot.TextWrapped = true
+	shoot.Text = "Grab Gun"
+	shoot.Parent = screenGui
+	
+	local shootbord = Instance.new("UIStroke")
+	shootbord.Thickness = 1
+	shootbord.Color = Color3.new(1, 1, 1)
+	shootbord.Parent = shoot
+	shootbord.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+	
+	shoot.MouseButton1Click:Connect(function()
+		clik() bringgun()
+	end)
+	
+	local esp = Instance.new("TextButton")
+	esp.Size = UDim2.new(0, 70, 0, 55)
+	esp.TextStrokeTransparency = 1
+	repos(esp, 70, 55, 80)
+	esp.BackgroundTransparency = 0.3
+	esp.TextColor3 = Color3.fromRGB(255, 255, 255)
+	esp.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	esp.Font = Enum.Font.Code
+	esp.BorderSizePixel = 0
+	esp.TextSize = 13
+	esp.TextXAlignment = Enum.TextXAlignment.Center
+	esp.TextYAlignment = Enum.TextYAlignment.Center
+	esp.Active = true
+	esp.Draggable = true
+	esp.TextWrapped = true
+	esp.Text = "Toggle\nESP (Off)"
+	esp.Parent = screenGui
+
+	local espbord = Instance.new("UIStroke")
+	espbord.Thickness = 1
+	espbord.Color = Color3.new(1, 1, 1)
+	espbord.Parent = esp
+	espbord.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+	
+	esp.MouseButton1Click:Connect(function()
+		clik()
+		task.spawn(function() if not highloop then startplayeresp() else stopplayeresp() end end)
+		task.spawn(function() if not nameloop then startnameesp() else stopnameesp() end end)
+		task.spawn(function() if not gunespactive then gundropesp() esp.Text = "Toggle\nESP (On)" else nogundropesp() esp.Text = "Toggle\nESP (Off)" end end)
+	end)
+	
+	local grabgun = Instance.new("TextButton")
+	grabgun.Size = UDim2.new(0, 70, 0, 55)
+	grabgun.TextStrokeTransparency = 1
+	repos(grabgun, 70, 55, -80)
+	grabgun.BackgroundTransparency = 0.3
+	grabgun.TextColor3 = Color3.fromRGB(255, 255, 255)
+	grabgun.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	grabgun.Font = Enum.Font.Code
+	grabgun.BorderSizePixel = 0
+	grabgun.TextSize = 13
+	grabgun.TextXAlignment = Enum.TextXAlignment.Center
+	grabgun.TextYAlignment = Enum.TextYAlignment.Center
+	grabgun.Active = true
+	grabgun.Draggable = true
+	grabgun.TextWrapped = true
+	grabgun.Text = "Shoot Murderer"
+	grabgun.Parent = screenGui
+
+	local grabgunbord = Instance.new("UIStroke")
+	grabgunbord.Thickness = 1
+	grabgunbord.Color = Color3.new(1, 1, 1)
+	grabgunbord.Parent = grabgun
+	grabgunbord.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+	
+	grabgun.MouseButton1Click:Connect(function()
+		clik()
+		if equipgun() then  
+			for i = 1, 11 do  
+				task.spawn(function()  
+					shootmurderer()  
+				end)  
+			end  
+		end
+	end)
+	
+	local killsheriff = Instance.new("TextButton")
+	killsheriff.Size = UDim2.new(0, 70, 0, 55)
+	killsheriff.TextStrokeTransparency = 1
+	repos(killsheriff, 70, 55, -160)
+	killsheriff.BackgroundTransparency = 0.3
+	killsheriff.TextColor3 = Color3.fromRGB(255, 255, 255)
+	killsheriff.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	killsheriff.Font = Enum.Font.Code
+	killsheriff.BorderSizePixel = 0
+	killsheriff.TextSize = 13
+	killsheriff.TextXAlignment = Enum.TextXAlignment.Center
+	killsheriff.TextYAlignment = Enum.TextYAlignment.Center
+	killsheriff.Active = true
+	killsheriff.Draggable = true
+	killsheriff.TextWrapped = true
+	killsheriff.Text = "Kill Sheriff"
+	killsheriff.Parent = screenGui
+
+	local killsheriffbord = Instance.new("UIStroke")
+	killsheriffbord.Thickness = 1
+	killsheriffbord.Color = Color3.new(1, 1, 1)
+	killsheriffbord.Parent = killsheriff
+	killsheriffbord.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+	killsheriff.MouseButton1Click:Connect(function()
+		clik()
+		stabsheriff()
+	end)
+end
+
+-------------------------------------------------------------------------------------------------------------------------------
